b0a9b28502d572277117c33827fbd7117acc161a
Harsha <harshavadluri.dev@gmail.com>
2024-11-21 16:13:41 +0530

    add images to /recens

--- components/Recens.tsx
+++ components/Recens.tsx
@@ -35,26 +35,132 @@ export default function Recens() {
           </div>
           <div className="flex flex-col">
             <h2 className="text-[30px] font-normal font-zcool">Timeline</h2>
-            <p>November – December 2022</p>
+            <p>1st November - 7st November 2024</p>
           </div>
         </section>
-        <p className="p-4">
-          The Recens logo utilizes Tenor Sans to convey a sense of modern
-          sophistication, while the tagline &quot;One of a Kind&quot; is
-          elegantly presented in Monterchi Sans, highlighting the brand&apos;s
-          uniqueness and artistic flair. These typefaces were selected to
-          enhance the visual appeal and ensure that every aspect of Recens’
-          identity resonates with our core values of elegance and individuality.
+        <p className="p-4 text-[20px]">
+          In this section, I have illustrated how creativity and strategic
+          design led us to our logo, thank-you cards, tags, and stickers. I
+          enjoy discovering Recens&apos;s essence as much as I enjoyed creating
+          it.
         </p>
 
         <section className="p-4">
-          <h2 className="text-[30px] font-normal font-zcool">Typography</h2>
-          <p className="font-zcool text-center">
-            In this section, I have illustrated how creativity and strategic
-            design led us to our logo, thank-you cards, tags, and stickers. I
-            enjoy discovering Recens&apos;s essence as much as I enjoyed
-            creating it.
+          <h2 className="text-[30px] font-normal font-zcool">Logo & Tagline</h2>
+          <p className="font-zcool text-[20px]">
+            The Recens logo utilizes Tenor Sans to convey a sense of modern
+            sophistication, while the tagline &quot;One of a Kind&quot; is
+            elegantly presented in Monterchi Sans, highlighting the brand&apos;s
+            uniqueness and artistic flair. These typefaces were selected to
+            enhance the visual appeal and ensure that every aspect of Recens’
+            identity resonates with our core values of elegance and
+            individuality.
           </p>
+          <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-[150px] mt-10">
+            <div className="flex-1 max-w-md shadow-2xl">
+              <Image
+                src="/1.png"
+                alt="recens logo"
+                width={400}
+                height={400}
+                className="object-contain w-auto h-full"
+              />
+            </div>
+            <div className="flex-1 max-w-md shadow-2xl">
+              <Image
+                src="/2.png"
+                alt="recens logo"
+                width={400}
+                height={400}
+                className="object-contain w-auto h-full"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="p-4">
+          <h2 className="text-[30px] font-normal font-zcool">
+            Thank-You Cards
+          </h2>
+          <p className="font-zcool text-[20px]">
+            My Desgin for Recens&apos; thank you cards embodies gratitude with
+            style. It blends minimalism with a personal touch, ensuring each
+            recipient feels uniquely appreciated.
+          </p>
+          <div className="flex flex-col md:flex-col justify-center items-center space-y-4 md:space-y-[40px] md:space-x-0 mt-10">
+            <div className="flex-1 max-w-full shadow-2xl">
+              <Image
+                src="/thankyoucard1.jpeg"
+                alt="recens logo"
+                width={1080}
+                height={786}
+                className="object-contain w-auto h-full"
+              />
+            </div>
+            <div className="flex-1 max-w-full shadow-2xl">
+              <Image
+                src="/thankyoucard2.jpeg"
+                alt="recens logo"
+                width={1080}
+                height={786}
+                className="object-contain w-auto h-full"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="p-4">
+          <h2 className="text-[30px] font-normal font-zcool">
+            Package Designing
+          </h2>
+          <p className="font-zcool text-[20px]">
+            Crafting understated elegance, my design for budget friendly Recens
+            corrugated boxes combines minimalism with functional luxury,
+            emphasizing a clean, sophisticated aesthetic.
+          </p>
+          <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-[150px] mt-10">
+            <div className="flex-1 max-w-md shadow-lg">
+              <Image
+                src="/recenspackagewireframe.png"
+                alt="recens logo"
+                width={800}
+                height={800}
+                className="object-contain w-auto h-full"
+              />
+            </div>
+            <div className="flex-1 max-w-md shadow-2xl">
+              <Image
+                src="/packagedesign2.png"
+                alt="recens logo"
+                width={800}
+                height={800}
+                className="object-contain w-full h-auto"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="p-4">
+          <h2 className="text-[30px] font-normal font-zcool">
+            Tags & Stickers
+          </h2>
+          <div className="flex flex-col md:flex-col justify-center items-center space-y-4 md:space-y-[40px] md:space-x-0 mt-10">
+            <div className="flex-1 max-w-full shadow-2xl">
+              <Image
+                src="/tagspic1.jpeg"
+                alt="recens logo"
+                width={1080}
+                height={786}
+                className="object-contain w-auto h-full"
+              />
+            </div>
+            <div className="flex-1 max-w-full shadow-2xl">
+              <Image
+                src="/thankyousticker.jpg"
+                alt="recens logo"
+                width={1080}
+                height={786}
+                className="object-contain w-auto h-full"
+              />
+            </div>
+          </div>
         </section>
       </div>
     </main>
